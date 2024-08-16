735aaf0a508f6926b1153bc1dad91b698f13c1cd
suresh <sureshveeramalla4@gmail.com>
2024-08-16 21:48:42 +0530

    Update apiCallUsingXMLHttpReq.js

--- apiCallUsingXMLHttpReq.js
+++ apiCallUsingXMLHttpReq.js
@@ -1,3 +1,10 @@
+/*Making API call Using plain JS  - XMLHttprequest
+-> onLoad: Used to define what should happen when api call is completed.
+-> xhr.send(): Used to send the request that have configures with api uri(xhr.open).
+-> for get it is simply xhr.send().
+-> for post xhr.send(queryParams)
+*/
+
 
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://dummyjson.com/users', true);
@@ -7,4 +14,9 @@ xhr.onload = function () {
     console.log(JSON.parse(xhr.responseText));
   }
 };
+
+xhr.onerror = function() {
+  // for handling network error
+  console.log(''Request failed);
+}
 xhr.send();
