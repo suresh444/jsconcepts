577e07755a69e4e5bb00429871ae4ead84cdb347
suresh <sureshveeramalla4@gmail.com>
2024-08-16 21:42:49 +0530

    Update apiCallUsingXMLHttpReq.js
    Api call using XMLHTTP Request

--- apiCallUsingXMLHttpReq.js
+++ apiCallUsingXMLHttpReq.js
@@ -1,1 +1,10 @@
 
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://dummyjson.com/users', true);
+xhr.onload = function () {
+  if (xhr.status >= 200) {
+    console.log(JSON.parse(xhr.responseText));
+    console.log(JSON.parse(xhr.responseText));
+  }
+};
+xhr.send();
